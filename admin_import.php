6cb60fdf94506d025869d208c928a4cf54506699
mahadihasanjewel57-bit <mahadihasanjewel57@gmail.com>
2026-06-03 15:42:11 +0600

    Update admin_import.php

--- admin_import.php
+++ admin_import.php
@@ -180,8 +180,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['excel_file'])) {
     <div class="nav-links">
         <a href="admin_dashboard.php?token=<?= $t ?>">Dashboard</a>
         <a href="admin_attendance.php?token=<?= $t ?>">Attendance</a>
-        <a href="admin_employees.php?token=<?= $t ?>">Employees</a>
-        <a href="admin_employees_edit.php?token=<?= $t ?>">Edit Employees</a>
+       <a href="admin_employees_edit.php?token=<?= $t ?>">Edit Employees</a>
         <a href="admin_device.php?token=<?= $t ?>">Devices</a>
         <a href="admin_import.php?token=<?= $t ?>">Import</a>
         <a href="admin_login.php">Logout</a>
